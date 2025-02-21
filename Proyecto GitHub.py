b589990ce0b4e10caf0a7cc43d445a299b0d617c
dxxmixn16 <damian.rodriguez2026@hilasal.superate.org.sv>
2025-02-21 08:58:37 -0600

    Ya no más a los guion bajos por favor :ambulance :

--- Proyecto GitHub.py
+++ Proyecto GitHub.py
@@ -3,5 +3,11 @@ import random
 equipos = ["Barcelona", "Real Madrid", "Manchester United", "Bayern Munich", "Juventus", "PSG", "Liverpool", "Chelsea"]
 historial = []
 
+def simular_partido():
+    e1, e2 = random.sample(equipos, 2)
+    g1, g2 = random.randint(0, 7), random.randint(0, 7)
+    resultado = f"{e1} {g1} - {g2} {e2}"
+    historial.append(resultado)
+    print(resultado)
 
 
